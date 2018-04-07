9d38971a820b18eed7d002e9bee67ae04f3d6f25
shark <sharkshore@163.com>
2018-04-07 08:41:31 +0800

    购物车页面

--- pages/merchants/merchants.js
+++ pages/merchants/merchants.js
@@ -1,18 +1,82 @@
 // pages/merchants/merchants.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+  data:{
+    products:[],//产品
+  },
+
   
+
+  getProducts(){
+    let products = [
+      {
+        productName: '迷迭香莹白亮彩眼霜',
+        price: '1635.00',
+        specification:'规格:白',
+        count:1,
+        productImageUrl: 'https://qiongxun-weiying.oss-cn-shanghai.aliyuncs.com/product/%E4%B8%BB%E5%9B%BE1.jpg'
+      },
+      {
+        productName: '护肤开元之作珍珠莹润',
+        price: '48.50',
+        specification: '规格:白',
+        count: 1,
+        productImageUrl: 'https://qiongxun-weiying.oss-cn-shanghai.aliyuncs.com/product/%E4%B8%BB%E5%9B%BE10.jpg'
+      },
+      {
+        productName: '春季唤醒面霜',
+        price: '89.00',
+        specification: '规格:白',
+        count: 1,
+        productImageUrl: 'https://qiongxun-weiying.oss-cn-shanghai.aliyuncs.com/product/%E4%B8%BB%E5%9B%BE2.jpg'
+      },
+      {
+        productName: '相宜本草修复露',
+        price: '159.00',
+        specification: '规格:白',
+        count: 2,
+        productImageUrl: 'https://qiongxun-weiying.oss-cn-shanghai.aliyuncs.com/product/%E4%B8%BB%E5%9B%BE3.jpg'
+      },
+      {
+        productName: '真尔美面霜',
+        price: '588.00',
+        specification: '规格:白',
+        count: 1,
+        productImageUrl: 'https://qiongxun-weiying.oss-cn-shanghai.aliyuncs.com/product/%E4%B8%BB%E5%9B%BE4.jpg'
+      },
+      {
+        productName: '相宜本草精华液',
+        price: '58.00',
+        specification: '规格:白',
+        count: 2,
+        productImageUrl: 'https://qiongxun-weiying.oss-cn-shanghai.aliyuncs.com/product/%E4%B8%BB%E5%9B%BE5.jpg'
+      },
+      {
+        productName: '膜法世家精制面膜',
+        price: '68.50',
+        specification: '规格:白',
+        count: 1,
+        productImageUrl: 'https://qiongxun-weiying.oss-cn-shanghai.aliyuncs.com/product/%E4%B8%BB%E5%9B%BE6.jpg'
+      },
+      {
+        productName: '魔术妹橄榄净透卸妆液',
+        price: '128.00',
+        specification: '规格:黑',
+        count: 2,
+        productImageUrl: 'https://qiongxun-weiying.oss-cn-shanghai.aliyuncs.com/product/%E4%B8%BB%E5%9B%BE7.jpg'
+      },
+    ]
+    return products;
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+      let products=this.getProducts()
+      this.setData({
+        products
+      })
   },
 
   /**
